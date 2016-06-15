bb1f67bd6505f77727f23b1a5a7ea6fc9dd714b4
Paul Laliberte <pala5552@colorado.edu>
2016-06-14 22:06:54 -0600

    Flask Framework

---
+++ web/app.py
@@ -0,0 +1,31 @@
+#This file houses all applications
+
+#import flask from Flask module
+from flask import Flask, render_template, redirect, url_for, request
+from flask.ext.bootstrap import Bootstrap
+
+
+
+#create an instant of an application object 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+#Use decorator "@" and route to url
+#More on decorators: http://flask.pocoo.org/docs/0.11/patterns/viewdecorators/
+@app.route('/')
+def home():
+    return render_template('bootstrap_temp.html')
+
+@app.route('/sampleHTML2')
+def another_page():
+    return render_template('sampleHTML2.html')
+
+@app.route('/tracking')
+def tracking_page():
+    return render_template('tracking.html')
+
+#start server
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
