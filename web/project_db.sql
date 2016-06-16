dea9a03d49a87aa568932ad179b8eb6a0e33a7bf
nicjohnston <nijo5747@colorado.edu>
2016-06-16 13:25:53 -0600

    Update project_db.sql

--- web/project_db.sql
+++ web/project_db.sql
@@ -14,7 +14,7 @@ INSERT INTO `Clients` (`Id`, `Name`, `Business`, `SenderLat`, `SenderLong` ) VAL
 (01, 'Will Christie', 'WTC', 39.7555, -105.2211),
 (02, 'Paul Laliberte', 'PRKL', 39.7392, -104.9903),
 (03, 'Kylee Budai', 'KMB', 30.0150, -105.2705),
-(04, 'Nicholas Johnston', 'NIJO', 39.9205, -105.0867),
+(04, 'Nicholas Johnston', 'NJJ', 39.9205, -105.0867),
 (05, 'Bill Christie', 'BTC', 39.8028, -105.0875);
 
 CREATE TABLE IF NOT EXISTS `Orders` (
