e86add5a699462dff1709db19e009c2524cd8977
PaulLaliberte <pala5552@colorado.edu>
2016-06-18 15:20:01 -0600

    all tables are connected

--- web/app.py
+++ web/app.py
@@ -8,17 +8,42 @@ from flask_sqlalchemy import SQLAlchemy
 #create an instant of an application object 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+
+#SQL-database-tables connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://paul:paul@localhost/project_db'
 db = SQLAlchemy(app)
 
 class Clients(db.Model):
     __tablename__ = 'Clients'
-    id = db.Column('id', db.Integer, primary_key = True)
+    id = db.Column('Id', db.Integer, primary_key = True)
     name = db.Column('Name', db.Unicode)
     password = db.Column('Password', db.Unicode)
     business = db.Column('Business', db.Unicode)
     latitude = db.Column('SenderLat', db.Numeric)
     longitude = db.Column('SenderLong', db.Numeric)
+
+class Drones(db.Model):
+    __tablename__ = 'Drones'
+    id = db.Column('Id', db.Integer, primary_key = True)
+    status = db.Column('Status', db.Integer)
+    details = db.Column('Details', db.Unicode)
+
+class OrderStatus(db.Model):
+    __tablename__ = 'OrderStatus'
+    id = db.Column('Status', db.Integer, primary_key = True)
+    description = db.Column('Description', db.Integer)
+
+class Orders(db.Model):
+    __tablename__ = 'Orders'
+    id = db.Column('OrderId', db.Integer, primary_key = True)
+    clientId = db.Column('ClientId', db.Integer)
+    droneId = db.Column('DroneId', db.Integer)
+    orderTimestamp = db.Column('OrderTimestamp', db.Integer)
+    receiverLat = db.Column('RecieverLat', db.Numeric)
+    receiverLong = db.Column('RecieverLong', db.Numeric)
+    status_ = db.Column('Status', db.Integer)
+
 
 
 #Use decorator "@" and route to url
@@ -26,14 +51,7 @@ class Clients(db.Model):
 @app.route('/')
 def home():
     return render_template('home.html')
-"""
-@app.route('/')
-def users():
-	cur = mysql.connection.cursor()
-	cur.execute('''SELECT user, host FROM mysql.user''')
-	rv = cur.fetchall()
-	return str(rv)
-"""
+
 @app.route('/register')
 def login():
      _name = request.form("name")
