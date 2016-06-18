4248a6cc91c57bc22c7d8423cf2717ad2deddcf0
kybu3653 <kybu3653@colorado.edu>
2016-06-17 22:23:40 -0600

    add mysql configurations

--- web/app.py
+++ web/app.py
@@ -5,9 +5,17 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flaskext.mysql import MySQL
 mysql = MySQL()
+
 #create an instant of an application object 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+#MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'project_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 #Use decorator "@" and route to url
 #More on decorators: http://flask.pocoo.org/docs/0.11/patterns/viewdecorators/
@@ -17,6 +25,8 @@ def home():
 
 @app.route('/register')
 def login():
+   _name = request.form("name")
+   _password = request.form("password")
    return render_template('register.html')
 
 @app.route('/sampleHTML2')
