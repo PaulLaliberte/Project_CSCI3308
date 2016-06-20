8ff755890223d9fb2fc19a29cb9b2c78e62f001d
Nicholas Johnston <nijo5747@colorado.edu>
2016-06-20 13:57:46 -0600

    Add register.php Equivalent to web/templates/register.html

---
+++ web/php/register.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> UAS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<div class = "navbar navbar-inverse" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+	<button type="button" class="navbar-toggle"
+	data-toggle="collapse" data-target=".navbar-collapse">
+	   <span class="sr.only">Toggle navigatio</span>
+	   <span class="icon-bar"></span>
+	   <span class="icon-bar"></span>
+	   <span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="/">Home</a>
+    </div>
+    <div class="navbar-collapse collapse">
+	<ul class="nav navbar-nav">
+	   <li><a href="/tracking">Tracking</a></li>
+	</ul>
+    </div>
+    </div>
+  </div>
+</div>
+
+    
+	<div class="container">
+		<div class="row main">
+         <div class="panel-heading">
+            <div class="panel-title text-center">
+               <h1 class="title">Register</h1>
+               <hr />
+            </div>
+         <div>
+			<div class="main-login main-center">
+				<div class="form-horizontal" method="post" action="#">
+					<div class="form-group">
+						<label for="name" class="cols-sm-2 control-label">Name</label>
+						<div class="cols-sm-10">
+							<div class="input-group">
+								<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+								<input type="text" class="form-control" name="name" id="name" placeholder="Login ID"></input>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="business" class="cols-sm-2 control-label">Business</label>
+						<div class="cols-sm-10">
+							<div class="input-group">
+								<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+								<input type="text" class="form-control" name="business" id="business" placeholder="Business Title"></input>
+							</div>
+						</div>
+					</div>
+			   	<div class="form-group">
+						<label for="address" class="cols-sm-2 control-label">Address</label>
+						<div class="cols-sm-10">
+							<div class="input-group">
+								<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+								<input type="text" class="form-control" name="address" id="address" placeholder="Business Address"></input>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="cols-sm-2 control-label">Password</label>
+						<div class="cols-sm-10">
+							<div class="input-group">
+								<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+								<input type="password" class="form-control" name="password" id="password" placeholder="Account Password" required></input>
+							</div>
+						</div>
+					</div>
+				<div class="form-group ">
+               <a class="btn btn-primary btn-lg btn-block" role="button" href="/">Register</a>
+                  </div>
+               
+               </form>	
+            </div>
+			</div>
+		</div>
+	</div>
+    </body>
+</div>
+
+
+    
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
