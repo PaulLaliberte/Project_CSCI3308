f473e45c57144dcbb43267af9b7c638d90c68a26
Nicholas Johnston <nijo5747@colorado.edu>
2016-06-20 14:06:02 -0600

    Add clientHome.php Equivalent to web/templates/clientHome.html

---
+++ web/php/clientHome.php
@@ -0,0 +1,103 @@
+
+<html>
+    <head>
+        <title>Client Homepage</title>
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <link href="static/bootstrap.min.css" rel="stylesheet" media="screen">
+        <style type="text/css">
+			.tg  {border-collapse:collapse;border-spacing:0;margin-left:auto;margin-right:auto;}
+			.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+			.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+			.tg .tg-yw4l{vertical-align:top}
+		</style>
+    </head>
+    <body>
+        <div class="container" style="margin-left:auto;margin-right:auto;text-align:center;">
+            <h1>Client Home</h2><br>
+            <table class="tg">
+			<tr>
+			<th class="tg-yw4l">Drone ID</th>
+			<th class="tg-yw4l">Drone Status</th>
+			<th class="tg-yw4l">Order ID</th>
+			<th class="tg-yw4l">Tracking Number</th>
+			<th class="tg-yw4l">Order status</th>
+			<th class="tg-yw4l">Notifications</th>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111111</td>
+			<td class="tg-yw4l">1</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111113</td>
+			<td class="tg-yw4l">1</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111114</td>
+			<td class="tg-yw4l">0</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111115</td>
+			<td class="tg-yw4l">0</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111116</td>
+			<td class="tg-yw4l">1</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111117</td>
+			<td class="tg-yw4l">1</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111118</td>
+			<td class="tg-yw4l">0</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			<tr>
+			<td class="tg-yw4l">111119</td>
+			<td class="tg-yw4l">2</td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			<td class="tg-yw4l"></td>
+			</tr>
+			
+			</table>
+            <p>Click <a href="/">here</a> to go home.</p>
+        </div>
+    </body>
+<html>
