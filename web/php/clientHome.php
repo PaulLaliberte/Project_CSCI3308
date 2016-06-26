0f1a7ac3bbe418247ad75601a17f1caaec0eb182
Nicholas Johnston <nijo5747@colorado.edu>
2016-06-26 14:38:15 -0600

    Copy order form back into clientHome.php

--- web/php/clientHome.php
+++ web/php/clientHome.php
@@ -208,6 +208,116 @@ if (!empty($_GET["address"]) && !empty($_GET["weight"]) && !empty($_GET["city"])
          ?>			
 			</table><br><br>
 	
+<div class="container">
+        <div class="row main">
+          <div class="panel-heading">
+            <div class="panel-title text-center">
+               <div name="order" id ="order" ><h3 class="title">Place Order</h3></div>
+               <hr />
+            </div>
+         </div>
+         <div class="main-login main-center">
+            <div class="form-horizontal">
+          <form action="" method="GET" id="orderForm">
+               <div class="form-group">
+                  <label for="address" class="cols-sm-2 control-label">Recipient Street</label>
+                  <div class="cols-sm-10">
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" name="address" placeholder="Street" autocomplete="street-address"></input>
+                     </div>
+                  </div>
+               </div>
+               <div class="form-group">
+                  <label for="city" class="cols-sm-2 control-label">Recipient City</label>
+                  <div class="cols-sm-10">
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" name="city" placeholder="City" autocomplete="address-level2"></input>
+                     </div>
+                  </div>
+               </div>
+               <div class="form-group">
+                  <label for="state" class="cols-sm-2 control-label">Recipient State</label>
+                  <div class="cols-sm-10">
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                        <input list="state" class="form-control" name="state" placeholder="State" autocomplete="address-level1"></input>
+                     </div>
+                  </div>
+               </div>
+            <datalist id='state' name="state">
+            <option value="Alaska">
+            <option value="Arizona">
+            <option value="Arkansas">
+            <option value="California">
+            <option value="Colorado">
+            <option value="Connecticut">
+            <option value="Delaware">
+            <option value="District Of Columbia">
+            <option value="Florida">
+            <option value="Georgia">
+            <option value="Hawaii">
+            <option value="Idaho">
+            <option value="Illinois">
+            <option value="Indiana">
+            <option value="Iowa">
+            <option value="Kansas">
+            <option value="Kentucky">
+            <option value="Louisiana">
+            <option value="Maine">
+            <option value="Maryland">
+            <option value="Massachusetts">
+            <option value="Michigan">
+            <option value="Minnesota">
+            <option value="Mississippi">
+            <option value="Missouri">
+            <option value="Montana">
+            <option value="Nebraska">
+            <option value="Nevada">
+            <option value="New Hampshire">
+            <option value="New Jersey">
+            <option value="New Mexico">
+            <option value="New York">
+            <option value="North Carolina">
+            <option value="North Dakota">
+            <option value="Ohio">
+            <option value="Oklahoma">
+            <option value="Oregon">
+            <option value="Pennsylvania">
+            <option value="Rhode Island">
+            <option value="South Carolina">
+            <option value="South Dakota">
+            <option value="Tennessee">
+            <option value="Texas">
+            <option value="Utah">
+            <option value="Vermont">
+            <option value="Virginia">
+            <option value="Washington">
+            <option value="West Virginia">
+            <option value="Wisconsin">
+            <option value="Wyoming" name="WY">
+            </datalist><br>
+               <div class="form-group">
+                  <label for="weight" class="cols-sm-2 control-label">Package Weight</label>
+                  <div class="cols-sm-10">
+                     <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                        <input list="text" class="form-control" name="weight" placeholder="Weight(kg)"></input>
+                     </div>
+                  </div>
+               </div>
+          <!--Pickup Time: <input type="datetime-local" name="pickupTime" value="<?php echo date("Y-m-d\TH:i:s"); ?>"><br>-->
+          Shipping Priority: <input type="radio" name="priority" checked="checked" value="high">High<br>
+               <div class="form-group ">
+                     <input type="submit" class="btn btn-primary btn-lg btn-block" value="Place Order">
+                  </div>
+        </form>
+      </div>
+      </div>
+   </div>
+</div>
+  
         <div class="container" id="wrapper">
           <div id="leftcolumn">
           <div class="panel-heading">
