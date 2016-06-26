fe430d00c18edf7d11724668cb43489c5ee45fd1
Paul Laliberte <pala5552@colorado.edu>
2016-06-26 15:10:42 -0600

    paypal button

--- web/php/clientHome.php
+++ web/php/clientHome.php
@@ -405,12 +405,19 @@ if (!empty($_GET["address"]) && !empty($_GET["weight"]) && !empty($_GET["city"])
 <!--  From paypal button creator -->
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="CKP5U8Q9DBZLG">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="hidden" name="hosted_button_id" value="SZX68WM8AQN7S">
+<table align="center">
+<tr><td><input type="hidden" name="on0" value="Drone Options">Drone Options</td></tr><tr><td><select name="os0">
+	<option value="Option 1">Option 1 $250.00 USD</option>
+	<option value="Option 2">Option 2 $500.00 USD</option>
+	<option value="Option 3">Option 3 $1,000.00 USD</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="USD">
+<input style="display: block; margin: 0 auto;" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
-
-   
                <div class="form-group ">
                      <input type="submit" class="btn btn-primary btn-lg btn-block" value="Process Request">
                </div>
